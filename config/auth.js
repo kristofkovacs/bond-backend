4f320ad08b682c85f2f41a3912f99f75ca16678b
Márk Tamás Dávid <davidmtamas@gmail.com>
2017-11-30 10:46:16 +0100

    - removed tracking of certain files

--- config/auth.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  'googleAuth' : {
-    'clientID': '665026689829-atmh1551a6tjcvrpt0kjiacneitvvrig.apps.googleusercontent.com',
-    'clientSecret': '229pa0OJhwuCaOW_Qw5fX-Lz',
-    'callbackURL': 'http://localhost:3000/google/auth/google/callback'
-  }
-};
